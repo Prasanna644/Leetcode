a9d6be941448ac514f6979d54ab468f574f80b2f
PrasannakumariLanka <honeyhoney2878@gmail.com>
2026-08-03 14:30:09 +0530

    Time: 4 ms (32.71%) | Memory: 19.3 MB (82.45%) - LeetSync

---
+++ 1378-cells-with-odd-values-in-a-matrix/cells-with-odd-values-in-a-matrix.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def oddCells(self, m: int, n: int, indices: List[List[int]]) -> int:
+        rows = [0] * m
+        cols = [0] * n
+
+        # Toggle the parity of rows and columns
+        for r, c in indices:
+            rows[r] ^= 1
+            cols[c] ^= 1
+
+        odd_rows = sum(rows)
+        odd_cols = sum(cols)
+
+        return odd_rows * (n - odd_cols) + (m - odd_rows) * odd_cols
